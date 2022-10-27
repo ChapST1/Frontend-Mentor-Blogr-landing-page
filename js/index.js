d5c1c70ef748498e6a8a371040ef2ab7e319fbf1
Christian <1368422@senati.pe>
2022-10-27 17:20:30 -0500

    endCode

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const hamburguer = document.querySelector('.nav__hamburguer')
+
+hamburguer.addEventListener('click', () => {
+    let nav = hamburguer.previousElementSibling;
+    nav.classList.toggle('nav-active')
+    if (nav.className.includes('nav-active')) {
+        hamburguer.setAttribute('src', "../images/icon-close.svg")
+    } else {
+        hamburguer.setAttribute('src', "../images/icon-hamburger.svg")
+    }
+})
